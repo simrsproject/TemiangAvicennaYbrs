054c38c875d39dccc5045ac07557f12b91b08590
Dikuy123-dev <andikuy1717@gmail.com>
2026-03-11 09:08:58 +0700

    Menu Baru Control Panel / User Dokter Asuransi

---
+++ Temiang.Avicenna/Module/ControlPanel/Admin/UserDokterAsuransi/UserDokterAsuransiDetail.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/MasterPage/MasterDetail.Master" AutoEventWireup="true"
+    CodeBehind="UserDokterAsuransiDetail.aspx.cs" Inherits="Temiang.Avicenna.Module.ControlPanel.Admin.UserDokterAsuransi.UserDokterAsuransiDetail" %>
+
+<%@ Register Assembly="Telerik.Web.UI" Namespace="Telerik.Web.UI" TagPrefix="telerik" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <script src="UserDokterAsuransiDetail.js" type="text/javascript"></script>
+
+    <table width="100%" cellpadding="0" cellspacing="0">
+        <tr>
+            <td style="width: 50%; vertical-align: top">
+                <table width="100%">
+                    <tr>
+                        <td class="label">
+                            <asp:Label ID="lblUserID" runat="server" Text="User ID"></asp:Label>
+                        </td>
+                        <td class="entry">
+                            <telerik:RadTextBox ID="txtUserID" runat="server" Width="100px" MaxLength="40" />
+                        </td>
+                        <td width="20">
+                            <asp:RequiredFieldValidator ID="rfvUserID" runat="server" ErrorMessage="User ID required."
+                                ValidationGroup="entry" ControlToValidate="txtUserID" SetFocusOnError="True"
+                                Width="100%">
+                                <asp:Image ID="Image1" runat="server" SkinID="rfvImage" />
+                            </asp:RequiredFieldValidator>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td class="label">
+                            <asp:Label ID="lblUserName" runat="server" Text="User Name"></asp:Label>
+                        </td>
+                        <td class="entry">
+                            <telerik:RadTextBox ID="txtUserName" runat="server" Width="300px" MaxLength="50" />
+                        </td>
+                        <td width="20">
+                            <asp:RequiredFieldValidator ID="rfvUserName" runat="server" ErrorMessage="User Name required."
+                                ValidationGroup="entry" ControlToValidate="txtUserName" SetFocusOnError="True"
+                                Width="100%">
+                                <asp:Image ID="Image2" runat="server" SkinID="rfvImage" />
+                            </asp:RequiredFieldValidator>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td class="label">
+                            <asp:Label ID="lblParamedicID" runat="server" Text="Physician ID"></asp:Label>
+                        </td>
+                        <td class="entry">
+                            <telerik:RadComboBox ID="cboParamedicID" runat="server" Width="300px" AllowCustomText="true"
+                                Filter="Contains" />
+                        </td>
+                        <td width="20"></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+
+    <telerik:RadCodeBlock ID="RadCodeBlock1" runat="server">
+        <script type="text/javascript">
+            //<![CDATA[
+            serverID("ajaxManagerID", "<%= AjaxManager.ClientID %>");
+        //]]>
+        </script>
+
+    </telerik:RadCodeBlock>
+</asp:Content>
---
+++ Temiang.Avicenna/Module/ControlPanel/Admin/UserDokterAsuransi/UserDokterAsuransiList.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Data;
+using Telerik.Web.UI;
+using Temiang.Avicenna.BusinessObject;
+using Temiang.Avicenna.Common;
+
+namespace Temiang.Avicenna.Module.ControlPanel.Admin.UserDokterAsuransi
+{
+    public partial class UserDokterAsuransiList : BasePageList
+    {
+        public override void OnMenuEditClick(Telerik.Web.UI.GridDataItem[] dataItems)
+        {
+            RedirectToPageDetail(dataItems[0], "edit");
+        }
+
+        public override void OnMenuViewClick(Telerik.Web.UI.GridDataItem[] dataItems)
+        {
+            RedirectToPageDetail(dataItems[0], "view");
+        }
+
+        private void RedirectToPageDetail(GridDataItem dataItem, string mode)
+        {
+            string id = dataItem.GetDataKeyValue(AppUserMetadata.ColumnNames.UserID).ToString();
+            Page.Response.Redirect("UserDokterAsuransiDetail.aspx?md=" + mode + "&id=" + id, true);
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            //UrlPageSearch = "UserSearch.aspx";
+            UrlPageDetail = "UserDokterAsuransiDetail.aspx";
+
+            ProgramID = AppConstant.Program.UserDokterAsuransi;
+
+            // Quick Search
+            ToolBarMenuQuickSearch.Visible = false;
+
+            ToolBarMenuSearch.Visible = false;
+
+            //Window Search
+            //WindowSearch.Height = 120;
+        }
+
+        protected void grdList_NeedDataSource(object source, Telerik.Web.UI.GridNeedDataSourceEventArgs e)
+        {
+            grdList.DataSource = AppUsersDokterAsuransi;
+        }
+
+        private DataTable AppUsersDokterAsuransi
+        {
+            get
+            {
+                object obj = Session[SessionNameForList];
+                if (obj != null)
+                    return ((DataTable)(obj));
+
+                AppUserQuery query;
+                if (Session[SessionNameForQuery] != null)
+                    query = (AppUserQuery)Session[SessionNameForQuery];
+                else
+                {
+                    query = new AppUserQuery("u");
+                    var par = new ParamedicQuery("p");
+
+                    query.LeftJoin(par).On(query.ParamedicID == par.ParamedicID);
+
+                    // 🔹 filter hanya user yang login
+                    query.Where(query.UserID == AppSession.UserLogin.UserID);
+
+                    query.Select(query, par.ParamedicName);
+
+                    //Quick Search
+                    //ApplyQuickSearch(query);
+                }
+
+                query.es.Top = AppSession.Parameter.MaxResultRecord;
+                DataTable dtb = query.LoadDataTable();
+                Session[SessionNameForList] = dtb;
+                return dtb;
+            }
+        }
+        //protected void grdList_DetailTableDataBind(object source, GridDetailTableDataBindEventArgs e)
+        //
+            //GridDataItem dataItem = e.DetailTableView.ParentItem;
+            //string userID = dataItem.GetDataKeyValue("UserID").ToString();
+
+            //if (e.DetailTableView.Name.Equals("grdUserGroup"))
+            //{
+               //AppUserUserGroupQuery query = new AppUserUserGroupQuery("a");
+                //AppUserGroupQuery qb = new AppUserGroupQuery("b");
+               // query.InnerJoin(qb).On(query.UserGroupID == qb.UserGroupID);
+               // query.Where(query.UserID == userID);
+               // query.OrderBy(qb.UserGroupName.Ascending);
+
+               // query.Select
+                   // (
+                 //       query.UserGroupID, qb.UserGroupName
+                  //  );
+
+               // e.DetailTableView.DataSource = query.LoadDataTable();
+           // }
+           // else
+           // {
+           //     AppUserServiceUnitQuery query = new AppUserServiceUnitQuery("a");
+          //      ServiceUnitQuery qb = new ServiceUnitQuery("b");
+            //    query.InnerJoin(qb).On(query.ServiceUnitID == qb.ServiceUnitID);
+            //    query.Where(query.UserID == userID, qb.IsActive == true);
+            //    query.OrderBy(qb.ServiceUnitName.Ascending);
+
+             //   query.Select
+             //       (
+             //           query.ServiceUnitID, qb.ServiceUnitName
+             //       );
+
+              //  e.DetailTableView.DataSource = query.LoadDataTable();
+           // }
+       // }
+    }
+}
